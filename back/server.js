d558b697415668a6f145927b02a261ddfb8685c6
Leva Antoine <antoine_leva@ens.univ-artois.fr>
2023-04-05 09:07:50 +0200

    ajout delete pour la seance #5

--- back/server.js
+++ back/server.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+
+const salle = require("./routes/route_salle");
 const routes = require("./routes/routes");
 const route_login = require("./routes/route_login");
-const film = require("./routes/route_film")
+const seance = require("./routes/route_seance")
+
 
 const app = express();
 const port = process.env.PORT || 8000;
@@ -15,6 +18,8 @@ app
     .use(express.json())
     .use(express.urlencoded({extended: true}))
     .use(routes)
+    .use(salle)
+    .use(seance)
     .use(route_login)
     .use((req, res) => {
         res.status(404);
--- front/src/Seance/Seance.js
+++ front/src/Seance/Seance.js
@@ -11,8 +11,7 @@ export default function Seance(props){
     let eventGuid = 0
     let todayStr = new Date().toISOString().replace(/T.*$/, '')
 
-    const [Seance, setSeance] = useState([]);
-    const INITIAL_EVENTS = [
+    const [Seance, setSeance] = useState([
         {
             id: createEventId(),
             title: 'All-day event',
@@ -29,9 +28,9 @@ export default function Seance(props){
             title: 'Timed event 300min',
             start: todayStr + 'T12:00:00'
         }
-    ]
+    ]);
 
-    function handleDateSelect(selectInfo) {
+    const handleDateSelect = (selectInfo) => {
         let title = prompt('Please enter a new title for your event')
         let calendarApi = selectInfo.view.calendar
 
@@ -48,26 +47,56 @@ export default function Seance(props){
         }
     }
 
-    function handleEventClick(clickInfo){
-        if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
-            clickInfo.event.remove()
+    const handleEventDelete = (clickInfo) => {
+        const eventId = clickInfo.event.id;
+        const eventStart = clickInfo.event.start;
+        const eventEnd = clickInfo.event.end;
+        if (window.confirm(`Êtes-vous sûr de vouloir supprimer le créneau du "${eventStart}" au "${eventEnd}" ?`)) {
+            console.log(eventId);
+            deleteEvent(eventId);
+            clickInfo.event.remove();
+            window.location.reload();
         }
     }
 
 
     async function getSeance() {
-        const response = await axios.request({
-            url: "http://localhost:8000/Seance",
-        }).then(
-            setSeance(response.data)
-        ).catch (error => {
+        try {
+            const response = await axios.request({
+                url: "http://localhost:8000/Seance",
+            })
+            setSeance(response.data);
+        } catch (error) {
             console.log("error", error);
-        },[])
-        const seanceEvents = Seance.map(a=>({
-            title:"test",
-            start: a.date_seance
-        }))
+        }
     }
+
+    async function handleSubmitSalle(e) {
+        e.preventDefault();
+        try {
+
+            const response = (await axios.post("http://localhost:8000/Seance" , Seance  ) ).data;
+
+
+            setSeance({date_seance: ""});
+            document.location.replace("http://localhost:3000/Seance");
+        } catch (e) {
+            console.error("ERR", e);
+        }
+    }
+
+    const deleteEvent = (id) => {
+        axios
+            .delete(`http://localhost:8000/Seance/${id}`)
+            .then((response) => {
+                console.log(response.data);
+                const updatedEvents = Seance.filter((event) => event.id !== id);
+                setSeance(updatedEvents);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
 
     useEffect(() => {
         (async () => {
@@ -80,7 +109,7 @@ export default function Seance(props){
     }
 
 
-    function renderEventContent(eventInfo) {
+    const renderEventContent = (eventInfo) => {
         return (
             <>
                 <b>{eventInfo.timeText}</b>
@@ -94,7 +123,7 @@ export default function Seance(props){
                 <Container className="mt-5">
                     <FullCalendar plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin ]}
                                       initialView="timeGridWeek"
-                                      events={INITIAL_EVENTS}
+                                      events={Seance}
                                       editable={true}
                                       selectable={true}
                                       selectMirror={true}
@@ -103,7 +132,8 @@ export default function Seance(props){
                                       locale={frLocale}
                                       select={handleDateSelect}
                                       eventContent={renderEventContent}
-                                      eventClick={handleEventClick}
+                                      eventClick={handleEventDelete}
+                                      eventAdd={handleSubmitSalle}
                     />
                 </Container>
             </div>
